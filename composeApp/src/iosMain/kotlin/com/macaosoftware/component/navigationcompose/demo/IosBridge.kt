12dd916b317509d2459e9dba2d1a118e54fedcc9
Pablo Valdes <am.pablo.vc@gmail.com>
2024-05-22 01:12:46 -0400

    Fix iOS and JS builds

---
+++ composeApp/src/iosMain/kotlin/com/macaosoftware/component/navigationcompose/demo/IosBridge.kt
@@ -0,0 +1,6 @@
+package com.macaosoftware.component.navigationcompose.demo
+
+@ObjCName(name = "IosBridge", exact = true)
+class IosBridge(
+    // val accountPlugin: AccountPlugin = AccountPluginEmpty()
+)
