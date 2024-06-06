984ce7cbbf6a17eb718ef6d16c65ff0bf3359205
Pablo Valdes <am.pablo.vc@gmail.com>
2024-06-06 02:53:33 -0400

    Remove DrawerComponentDefaults class

--- composeApp/src/commonMain/kotlin/com.macaosoftware.component.navigationcompose.demo/di/DrawerViewModelsModule.kt
+++ composeApp/src/commonMain/kotlin/com.macaosoftware.component.navigationcompose.demo/di/DrawerViewModelsModule.kt
@@ -1,23 +1,43 @@
 package com.macaosoftware.component.navigationcompose.demo.di
 
 import com.macaosoftware.component.core.RootDestinationRender
-import com.macaosoftware.component.drawer.DrawerComponentDefaults
+import com.macaosoftware.component.drawer.DrawerHeaderDefaultState
+import com.macaosoftware.component.drawer.DrawerHeaderState
 import com.macaosoftware.component.drawer.DrawerStatePresenterDefault
+import com.macaosoftware.component.drawer.DrawerStyle
 import com.macaosoftware.component.navigationcompose.demo.marketplace.navigators.drawer.DemoDrawerRootDestinationRender
 import com.macaosoftware.component.navigationcompose.demo.marketplace.navigators.drawer.DemoDrawerViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import org.koin.compose.viewmodel.dsl.viewModelOf
-import org.koin.core.module.dsl.factoryOf
+import org.koin.core.parameter.ParametersHolder
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
 internal val drawerViewModelsModule = module {
 
-    factory<DrawerStatePresenterDefault> {
-        DrawerComponentDefaults.createDrawerStatePresenter()
+    // Drawer Presenter
+    factory<DrawerStatePresenterDefault> { parameters: ParametersHolder ->
+
+        val drawerStyle = DrawerStyle()
+
+        val drawerHeaderState = DrawerHeaderDefaultState(
+            title = "Header Title",
+            description = "This is the default text. Provide your own text for your App",
+            imageUri = "",
+            style = drawerStyle
+        )
+
+        DrawerStatePresenterDefault(
+            dispatcher = parameters.getOrNull<CoroutineDispatcher>() ?: Dispatchers.Main,
+            drawerHeaderState = parameters.getOrNull<DrawerHeaderState>() ?: drawerHeaderState,
+            drawerStyle = drawerStyle
+        )
     }
 
     // ViewModels
     viewModelOf(::DemoDrawerViewModel)
+    // TODO: Use injection with parameters when creating bellow ViewModel
     /*viewModel<DemoDrawerViewModel> { parameters: ParametersHolder ->
         DemoDrawerViewModel(
             route = parameters.get(),
